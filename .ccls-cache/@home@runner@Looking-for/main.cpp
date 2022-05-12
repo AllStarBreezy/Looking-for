1a6c7e724ae0a48906a3be4bfc3a534f02ee8d01
Breezy <86167887+AllStarBreezy@users.noreply.github.com>
2022-05-12 17:42:21 +0000

    Loop Iterations w/ Inner Loop iterations

--- .ccls-cache/@home@runner@Looking-for/main.cpp
+++ .ccls-cache/@home@runner@Looking-for/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-  std::cout << "Hello World!\n";
+int i, j;
+
+  for (i = 1 ; i < 4 ; i++)
+    {
+      cout << "Loop iteration: " << i << endl;
+      for (j = 1 ; j < 4 ; j++)
+        {
+          cout << "  Inner Loop iteration: " << j << endl;
+        }
+    }
+  return 0;
 }
